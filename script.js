42a63d9472c61b5b33bedda4ed4dba2f8f86f9ed
jossua2602 <jossuabongo2602@gmail.com>
2026-02-08 18:54:23 +0800

    lab02

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ form.addEventListener("submit", (e) => {
 
     
 });
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+ 
+    const name = this.name1.value;
+    const email = this.email1.value;
+    const message = this.message1.value;
+    const date = new Date().toLocaleString();
+
+   
+    const newSubmission = {
+        date: date,
+        name: name,
+        email: email,
+        message: message
+    };
+
+ 
+    const submissions = JSON.parse(localStorage.getItem('allSubmissions')) || [];
+
+  
+    submissions.push(newSubmission);
+
+   
+    localStorage.setItem('allSubmissions', JSON.stringify(submissions));
+
+   
+
+    this.reset();
+});
