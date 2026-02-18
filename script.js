2d05ec786a576b4e60094b0dc910fd1e0419711d
jblair <devilajb@gmail.com>
2026-02-18 21:28:24 +0800

    commit

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
         loader.style.display = "none";
       }, 800);
 
-    }, 1000);
+    }, 8000);
   });
 
 
@@ -86,3 +86,4 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
 
     this.reset();
 });
+
