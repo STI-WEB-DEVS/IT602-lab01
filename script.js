3827672ffe7019c18df32e65e6e44d6fd1e635e6
jblair <devilajb@gmail.com>
2026-02-18 21:25:27 +0800

    commit

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+// Loader
+ const loader = document.getElementById("loader");
+
+  window.addEventListener("DOMContentLoaded", () => {
+    loader.style.transition = "opacity 0.8s ease";
+    loader.style.opacity = "1";
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      loader.style.opacity = "0";
+
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 800);
+
+    }, 1000);
+  });
+
+
+// Dark & Light Mode Toggle
+const toggleBtn = document.getElementById("theme-toggle");
+const html = document.documentElement;
+
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  toggleBtn.innerHTML = '<i class="fas fa-moon"></i>'; // dark mode icon
+} else {
+  html.classList.remove("dark");
+  toggleBtn.innerHTML = '<i class="fas fa-lightbulb"></i>'; // light mode icon
+}
+
+toggleBtn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.innerHTML = '<i class="fas fa-moon"></i>'; // dark mode icon
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.innerHTML = '<i class="fas fa-lightbulb"></i>'; // light mode icon
+  }
+});
+
+
+// Contact form handling
 const form = document.getElementById("contact-form");
 
 form.addEventListener("submit", (e) => {
@@ -33,11 +79,7 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
 
  
     const submissions = JSON.parse(localStorage.getItem('allSubmissions')) || [];
-
-  
     submissions.push(newSubmission);
-
-   
     localStorage.setItem('allSubmissions', JSON.stringify(submissions));
 
    
